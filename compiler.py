9cfa6039e30f767f17835435c2396ba0ed5e2ec1
Rishabh Kalakoti <rishabh.kalakoti@gmail.com>
2017-09-12 12:13:00 -0700

    night's work bfore lab

--- compiler.py
+++ compiler.py
@@ -7,15 +7,21 @@ code = open(fileName, 'r').read()
 
 # -------- getting file after preprocessor output
 code = pre.preprocessor(code)
-print(code)
-print("")
 # -------- getting output after lexical analysis
 symbolTable = dict()
 code, symbolTable = lex.lexer(code)
-print("")
-if(syntax.syntaxChecker(code) == False):
-	print(syntax.ERROR)
+#print("")
+if(syntax.syntaxChecker(code, symbolTable) == False):
+	# print(syntax.ERROR)
+	print("DEAD!!")
 	exit()
-print("GOOD TO GO!")
+
+print("-"*10, "LEX OUTPUT", "-"*10)
 print(code)
-print(symbolTable)
+print("")
+print("-"*10, "SYMBOL TABLE", "-"*10)
+for element in symbolTable:
+	print("#%s : %s" %(element,symbolTable[element]))
+print("")
+print("GOOD TO GO!")
+
--- syntax.py
+++ syntax.py
@@ -1,17 +1,13 @@
-"""
-	
-"""
-
 import re
 ERROR=""
 
-def syntaxChecker(code):
+def syntaxChecker(code, symbolTable):
 	if(not isStmtList(code)):
 		return False
 	return True
 
 def isStmtList(code):
-	# <func_dec> | <var_dec> | <control_dec>
+	# <func_def> | <func_dec> | <var_dec> | <control_dec>
 	# ----------- func def
 	funcDef = re.findall(r'\#FUNC.*?\#END\#SPACE\#FUNC', code)
 	for stmt in funcDef:
@@ -42,6 +38,13 @@ def isFuncDef(stmt):
 	return True
 
 def isIfStmt(ifStmt):
+	ifObj = re.match(r'\#IF\#LPAREN(.*)\#RPAREN(.*)\#END\#SPACE\#IF', ifStmt)
+	if not isLogicExp(ifObj.group(1)):
+		return False
+	if not isStmtList(ifObj.group(2)):
+		return False
+	if ifObj==None:
+		return False
 	return True
 
 def isLoopStmt(loopStmt):
@@ -51,24 +54,62 @@ def isStmt(stmt):
 	# ----------- <var_asst><function_call>
 	if stmt=="#SPACE":
 		return True
-	print(stmt)
 	if not (isFunctionCall(stmt) or isVarAsst(stmt)):
 		return False
 	return True
 
 def isVarAsst(stmt):
-	if stmt==None:
-		print("xxx")
-	print(re.match(r'\#VAR(\#SPACE)?\#ASSTOP.*$', stmt, re.I))
+	stmt=re.sub(r'\#SPACE', "", stmt)
+	if stmt=="":
+		return True
 	if not re.match(r'\#VAR(\#SPACE)?\#ASSTOP.*$', stmt, re.I):
 		return False
-	print(re.match(r'\#VAR(\#SPACE)?\#ASSTOP.*$', stmt, re.I))
+	if not isArithExp(re.match(r'\#VAR(\#SPACE)?\#ASSTOP(.*)$', stmt, re.I).group(2)):
+		return False
 	return True
 
 def isFunctionCall(stmt):
 	if not re.match(r'\#CALLOP\#FNAME\#LPAREN(.*)\#RPAREN$', stmt, re.I):
 		return False
-	print(re.match(r'\#CALLOP\#FNAME\#LPAREN(.*)\#RPAREN$', stmt, re.I))
-	#----------( incomplete )---------
-	print(stmt)
+	#----------( incomplete )---------arguments not done yet
+	
+	return True
+
+def isLogicExp(stmt):
+	if re.match(r'(\#SPACE)?\#NOT(\#SPACE)?(.*)(\#SPACE)?',stmt)!=None:
+		if isLogicExp(re.match(r'(\#SPACE)?\#NOT(\#SPACE)?(.*)(\#SPACE)?',stmt).group(3)):
+			return True
+		return False
+	if(re.match(r'\#LOGICOP',stmt)==None):
+		if not isRelExp(stmt):
+			return False
+	if re.search(r'\#LOGICOP', stmt)!=None:
+		for exp in re.split(r'\#LOGICOP', stmt):
+			if not (isLogicExp(exp)):
+				return False
+	if(stmt==""):
+		return False
+	return True
+
+def isRelExp(stmt):
+	stmt=re.sub(r'\#SPACE', "", stmt)
+	if stmt=="" or stmt==None:
+		return True
+	for exp in re.split(r'\#RELOP', stmt):
+		if not (isArithExp(exp)):
+			return False
+	return True
+
+def isArithExp(stmt):
+	for term in re.split(r'\#ARITHOP', stmt):
+		if not (isTerm(term)):
+			return False
+	return True
+
+def isTerm(term):
+	if re.match(r'\#VAR|\#NUM|#LPAREN.*\#RPAREN$', term)==None:
+		return False
+	if re.match(r'\#LPAREN(.*)\#RPAREN$', term)!=None:
+		if not isArithExp(re.match(r'\#LPAREN(.*)\#RPAREN$', term).group(1)):
+			return False
 	return True
